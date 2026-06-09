c43dae797806abb4e1e9923e9db77ce1e72bb179
Julius <young69071@gmail.com>
2026-06-09 13:07:37 +0300

    Add files via upload

--- formats.py
+++ formats.py
@@ -1,26 +1,48 @@
 # This file is distributed under the same license as the Django package.
 #
-DATE_FORMAT = r"j \d\e F \d\e Y"
-TIME_FORMAT = "H:i"
-DATETIME_FORMAT = r"j \d\e F \d\e Y \a \l\a\s H:i"
-YEAR_MONTH_FORMAT = r"F \d\e Y"
-MONTH_DAY_FORMAT = r"j \d\e F"
+# The *_FORMAT strings use the Django date format syntax,
+# see https://docs.djangoproject.com/en/dev/ref/templates/builtins/#date
+DATE_FORMAT = "N j, Y"
+TIME_FORMAT = "P"
+DATETIME_FORMAT = "N j, Y, P"
+YEAR_MONTH_FORMAT = "F Y"
+MONTH_DAY_FORMAT = "F j"
 SHORT_DATE_FORMAT = "d/m/Y"
-SHORT_DATETIME_FORMAT = "d/m/Y H:i"
-FIRST_DAY_OF_WEEK = 1
+SHORT_DATETIME_FORMAT = "d/m/Y P"
+FIRST_DAY_OF_WEEK = 0
+
+# The *_INPUT_FORMATS strings use the Python strftime format syntax,
+# see https://docs.python.org/library/datetime.html#strftime-strptime-behavior
 DATE_INPUT_FORMATS = [
-    "%d/%m/%Y",  # '25/10/2006'
-    "%d/%m/%y",  # '25/10/06'
-    "%Y%m%d",  # '20061025'
+    "%Y-%m-%d",  # '2006-10-25'
+    "%m/%d/%Y",  # '10/25/2006'
+    "%m/%d/%y",  # '10/25/06'
+    "%b %d %Y",  # 'Oct 25 2006'
+    "%b %d, %Y",  # 'Oct 25, 2006'
+    "%d %b %Y",  # '25 Oct 2006'
+    "%d %b, %Y",  # '25 Oct, 2006'
+    "%B %d %Y",  # 'October 25 2006'
+    "%B %d, %Y",  # 'October 25, 2006'
+    "%d %B %Y",  # '25 October 2006'
+    "%d %B, %Y",  # '25 October, 2006'
 ]
 DATETIME_INPUT_FORMATS = [
-    "%d/%m/%Y %H:%M:%S",
-    "%d/%m/%Y %H:%M:%S.%f",
-    "%d/%m/%Y %H:%M",
-    "%d/%m/%y %H:%M:%S",
-    "%d/%m/%y %H:%M:%S.%f",
-    "%d/%m/%y %H:%M",
+    "%Y-%m-%d %H:%M:%S",  # '2006-10-25 14:30:59'
+    "%Y-%m-%d %H:%M:%S.%f",  # '2006-10-25 14:30:59.000200'
+    "%Y-%m-%d %H:%M",  # '2006-10-25 14:30'
+    "%m/%d/%Y %H:%M:%S",  # '10/25/2006 14:30:59'
+    "%m/%d/%Y %H:%M:%S.%f",  # '10/25/2006 14:30:59.000200'
+    "%m/%d/%Y %H:%M",  # '10/25/2006 14:30'
+    "%m/%d/%y %H:%M:%S",  # '10/25/06 14:30:59'
+    "%m/%d/%y %H:%M:%S.%f",  # '10/25/06 14:30:59.000200'
+    "%m/%d/%y %H:%M",  # '10/25/06 14:30'
 ]
+TIME_INPUT_FORMATS = [
+    "%H:%M:%S",  # '14:30:59'
+    "%H:%M:%S.%f",  # '14:30:59.000200'
+    "%H:%M",  # '14:30'
+]
+
 DECIMAL_SEPARATOR = ","
-THOUSAND_SEPARATOR = "."
+THOUSAND_SEPARATOR = "\xa0"
 NUMBER_GROUPING = 3
